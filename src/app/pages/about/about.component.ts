cdd65d7a8896f80add96b422407693859000b46a
DevCompleteStudios <jorge.yael333@gmail.com>
2025-03-09 14:39:55 -0600

    Agregando paginas iniciales

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './about.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class AboutComponent {
+
+
+
+}
---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './contact.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class ContactPage {
+
+
+
+}
