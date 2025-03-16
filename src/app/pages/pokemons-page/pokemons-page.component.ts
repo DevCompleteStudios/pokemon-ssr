db0e1e85a7470b1d2ede7713588485ab893d47f8
DevCompleteStudios <jorge.yael333@gmail.com>
2025-03-16 09:53:04 -0600

    Actualizando query params

--- src/app/pages/pokemons-page/pokemons-page.component.ts
+++ src/app/pages/pokemons-page/pokemons-page.component.ts
@@ -1,32 +1,46 @@
-import { ApplicationRef, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/pokemon-list/pokemon-list.component";
-import { PokemonListSkeletonComponent } from "../../pokemons/pokemon-list-skeleton/pokemon-list-skeleton.component";
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { ISimplePokemon } from '../../pokemons/interfaces/ISimplePokemon';
+import { ActivatedRoute, Router } from '@angular/router';
+import {toSignal} from '@angular/core/rxjs-interop'
+import { map, tap } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pokemons-page',
-  imports: [PokemonListComponent, PokemonListSkeletonComponent],
+  imports: [PokemonListComponent],
   templateUrl: './pokemons-page.component.html'
 })
 export default class PokemonsPageComponent implements OnInit {
 
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private title = inject(Title);
   protected pokemons = signal<ISimplePokemon[]>([]);
-
-  constructor(
-    private pokemonsService:PokemonsService,
-  ){}
+  protected pokemonsService = inject(PokemonsService);
+  protected currentPage = toSignal<number>(this.route.queryParamMap.pipe(
+    map(res => res.get("page") ?? '1'),
+    map(page => (isNaN(+page) ? 1 : +page)),
+    map(p => Math.max(1, p))
+  ));
 
 
   ngOnInit(): void {
+    console.log(this.currentPage);
     this.loadPokemons();
   }
 
 
   public loadPokemons(page:number = 0){
-    this.pokemonsService.loadPage(page)
+    const pageLoad = this.currentPage()! + page;
+
+    this.pokemonsService.loadPage(pageLoad)
+      .pipe(
+        tap(() => this.router.navigate([], {queryParams: {page: pageLoad}})),
+        tap(() => this.title.setTitle("Pokemons SSR - page " + pageLoad))
+      )
       .subscribe(p => this.pokemons.set(p));
   }
-
 
 }
